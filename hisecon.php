8b1cecd6553fb63a6bf91626f8f19ed9b8791b64
Richard Neumann <r.neumann@homeinfo.de>
2016-05-31 16:11:20 +0200

    Started on hisecon.php implementation

--- hisecon.php
+++ hisecon.php
@@ -1,19 +1,21 @@
 <?php
 /* HOMEINFO secure contact form */
 
+/* Websites, authorized to use this mailer */
 $VALID_DOMAINS = (
 	'barrierefrei-wohnen-bremen.de',
 	'barrierefrei-wohnen-hannover.de');
 
+/* Calling host */
 $SRC_HOST = $_GET['HTTP_HOST'];
 
 $src_email = $_GET['src_email'];
 $reply_email = $_GET['reply_email'];
 $copy2recipient = $_GET['copy2recipient'];
 
-$name = $_GET['name'];
-$email = $_GET['email'];
-$msg = $_GET['msg'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
 
 if in_array($HTTP_HOST, $VALID_DOMAINS) {
 	/* Website is allowed to use the mailer */
