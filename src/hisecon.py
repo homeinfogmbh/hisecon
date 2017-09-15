b0c932651e764ab153c1b1d4d623ece16f3269aa
Richard Neumann <r.neumann@homeinfo.de>
2017-09-15 16:35:28 +0200

    Refactored hisecon.

--- src/hisecon.py
+++ src/hisecon.py
@@ -1,4 +1,4 @@
-"""HOMEINFO Secure Contact form
+"""HOMEINFO Secure Contact form.
 
 This project provides a secure web mailer using reCaptcha by Google Inc.
 and a token system to authenticate calling sites.
@@ -18,139 +18,90 @@ from wsgilib import escape_object, OK, Error, InternalServerError, \
 __all__ = ['Hisecon']
 
 
-class HiseconConfig(ConfigParserPlus):
-    """Configuration parser for hisecon"""
-
-    @property
-    def mail(self):
-        """Returns the mail section"""
-        return self['mail']
+JSON = '/etc/hisecon.json'
+CONFIG = ConfigParserPlus('/etc/hisecon.conf', alert=True)
 
 
 class Hisecon(RequestHandler):
-    """Handles requests of the hisecon app"""
-
-    JSON = '/etc/hisecon.json'
-    CONFIG = HiseconConfig('/etc/hisecon.conf', alert=True)
+    """ReCaptcha secured, configurable mail backend."""
 
     def __init__(self, *args, **kwargs):
-        """Load site once"""
+        """Loads the site configuration."""
         super().__init__(*args, **kwargs)
+        self.config = self._load_config()
 
+    def _load_config(self):
+        """Loads the site configuration JSON file."""
         try:
-            with open(self.JSON) as f:
-                json = f.read()
-        except FileNotFoundError:
-            self.logger.error('Sites file not found: {}'.format(self.JSON))
-            raise InternalServerError('Sites file not found.') from None
-        except PermissionError:
-            self.logger.error('Cannot read sites file: {}'.format(self.JSON))
-            raise InternalServerError('Sites file not readable.') from None
-        else:
-            try:
-                sites = loads(json)
-            except ValueError:
-                self.logger.error('Invalid content:\n{}'.format(json))
-                raise InternalServerError('Corrupted sites file.') from None
-            else:
-                try:
-                    self.site = sites[self.config]
-                except KeyError:
-                    msg = 'No such configuration: "{}"'.format(self.config)
-                    self.logger.warning(msg)
-                    raise Error(msg, status=400) from None
-                else:
-                    self.smtp = self.site.get('smtp', {})
-
-    @property
-    def remote_ip(self):
-        """Returns the specified remote IP address"""
-        return self.query.get('remoteip')
-
-    @property
-    def reply_to(self):
-        """Returns the optional reply-to address"""
-        return self.query.get('reply_to')
-
-    @property
-    def format(self):
-        """Returns the desired format"""
-        try:
-            return self.query['format']
-        except KeyError:
-            if self.query.get('html') is not None:
-                return 'html'
-            else:
-                return 'text'
-
-    @property
-    def config(self):
-        """Returns the specified configuration"""
-        try:
-            return self.query['config']
+            config = self.query['config']
         except KeyError:
             raise self.logerr('No configuration provided.') from None
 
-    @property
-    def secret(self):
-        """Returns the respective reCAPTCHA secret"""
         try:
-            return self.site['secret']
+            with open(JSON) as json:
+                text = json.read()
+        except FileNotFoundError:
+            self.logger.error('Sites file not found: {}.'.format(JSON))
+            raise InternalServerError('Sites file not found.') from None
+        except PermissionError:
+            self.logger.error('Cannot read sites file: {}.'.format(JSON))
+            raise InternalServerError('Sites file not readable.') from None
+
+        try:
+            sites = loads(text)
+        except ValueError:
+            self.logger.error('Invalid content:\n{}'.format(json))
+            raise InternalServerError('Corrupted sites file.') from None
+
+        try:
+            return sites[config]
         except KeyError:
-            raise self.logerr(
-                'No secret specified for configuration.', status=500) from None
+            msg = 'No such configuration: "{}".'.format(config)
+            self.logger.warning(msg)
+            raise Error(msg, status=400) from None
+
+    @property
+    def format(self):
+        """Returns the desired format."""
+        try:
+            return self.query['format']
+        except KeyError:
+            if self.query.get('html', False):
+                return 'html'
+
+            return 'text'
 
     @property
     def response(self):
-        """Returns the respective reCAPTCHA response"""
+        """Returns the respective reCAPTCHA response."""
         try:
             return self.query['response']
         except KeyError:
             raise self.logerr('No reCAPTCHA response provided.') from None
 
     @property
-    def issuer(self):
-        """Returns the optional issuer address"""
-        return self.query.get('issuer')
+    def recipients(self):
+        """Yields all recipients."""
+        for site_recipient in self.config.get('recipients', []):
+            yield site_recipient
 
-    @property
-    def default_recipients(self):
-        """Yields default recipients"""
         try:
-            return self.site['recipients']
-        except KeyError:
-            return []
-
-    @property
-    def additional_recipients(self):
-        """Yields the optional additional recipients"""
-        try:
-            recipients = self.query['recipients'].split(',')
+            recipients = self.query['recipients']
         except KeyError:
             pass
         else:
-            for recipient in recipients:
-                recipient = recipient.strip()
+            for query_recipient in recipients.split(','):
+                query_recipient = query_recipient.strip()
 
-                if recipient:
-                    yield recipient
+                if query_recipient:
+                    yield query_recipient
 
-        # Yield legacy single recipient argument
         with suppress(KeyError):
-            yield self.query['recipient']
-
-    @property
-    def recipients(self):
-        """Yields all recipients"""
-        yield from self.default_recipients
-        yield from self.additional_recipients
-
-        if self.issuer is not None:
-            yield self.issuer
+            yield self.query['issuer']
 
     @property
     def subject(self):
-        """Returns the respective subject"""
+        """Returns the respective subject."""
         try:
             subject = self.query['subject']
         except KeyError:
@@ -161,38 +112,16 @@ class Hisecon(RequestHandler):
             return unquote(subject)
 
     @property
-    def host(self):
-        """Returns the SMTP server's host name"""
-        return self.smtp.get('host', self.CONFIG.mail['HOST'])
-
-    @property
-    def port(self):
-        """Returns the SMTP server's port"""
-        return self.smtp.get('port', int(self.CONFIG.mail['PORT']))
-
-    @property
-    def user(self):
-        """Returns the SMTP user"""
-        return self.smtp.get('user', self.CONFIG.mail['USER'])
-
-    @property
-    def passwd(self):
-        """Returns the SMTP user's password"""
-        return self.smtp.get('passwd', self.CONFIG.mail['PASSWD'])
-
-    @property
-    def ssl(self):
-        """Returns the SMTP server's SSL/TLS settings"""
-        return self.smtp.get('ssl', True)
-
-    @property
     def sender(self):
-        """Returns the specified sender's email address"""
-        return self.smtp.get('from', self.CONFIG.mail['FROM'])
+        """Returns the specified sender's email address."""
+        try:
+            return self.config['smtp']['from']
+        except KeyError:
+            return CONFIG['mail']['FROM']
 
     @property
     def text(self):
-        """Return the POSTed text"""
+        """Return the POSTed text."""
         try:
             return unquote(self.data.decode())
         except AttributeError:
@@ -202,7 +131,7 @@ class Hisecon(RequestHandler):
 
     @property
     def json(self):
-        """Returns the POSTed JSON data"""
+        """Returns the POSTed JSON data."""
         try:
             return loads(self.data.decode())
         except AttributeError:
@@ -212,13 +141,13 @@ class Hisecon(RequestHandler):
 
     @property
     def template(self):
-        """Returns the optional template"""
-        file_name = '{}.temp'.format(self.site['template'])
+        """Returns the optional template."""
+        file_name = '{}.temp'.format(self.config['template'])
         file_path = join('/usr/share/hisecon', file_name)
 
         try:
-            with open(file_path, 'r') as f:
-                return f.read()
+            with open(file_path, 'r') as file:
+                return file.read()
         except FileNotFoundError:
             raise InternalServerError('Template not found.') from None
         except PermissionError:
@@ -226,14 +155,14 @@ class Hisecon(RequestHandler):
 
     @property
     def body(self):
-        """Returns the emails plain text and HTML bodies"""
-        format = self.format
+        """Returns the emails plain text and HTML bodies."""
+        frmt = self.format
 
-        if format == 'html':
+        if frmt == 'html':
             return self.text
-        elif format == 'text':
+        elif frmt == 'text':
             return self.text.replace('<br>', '\n')
-        elif format == 'json':
+        elif frmt == 'json':
             json = escape_object(self.json)
 
             try:
@@ -248,17 +177,28 @@ class Hisecon(RequestHandler):
 
     @property
     def mailer(self):
-        """Returns an appropriate mailer"""
-        return Mailer(self.host, self.port, self.user, self.passwd,
-                      ssl=self.ssl, logger=self.logger)
+        """Returns an appropriate mailer."""
+        smtp = self.config.get('smtp', {})
+        host = smtp.get('host', CONFIG['mail']['HOST'])
+        port = smtp.get('port', int(CONFIG['mail']['PORT']))
+        user = smtp.get('user', CONFIG['mail']['USER'])
+        passwd = smtp.get('passwd', CONFIG['mail']['PASSWD'])
+        ssl = smtp.get('ssl', True)
+        return Mailer(host, port, user, passwd, ssl=ssl, logger=self.logger)
 
     @property
     def recaptcha(self):
-        """Returns a recaptcha client"""
-        return ReCaptcha(self.secret, logger=self.logger)
+        """Returns the ReCAPTCHA client."""
+        try:
+            secret = self.config['secret']
+        except KeyError:
+            raise self.logerr(
+                'No secret specified for configuration.', status=500) from None
+        else:
+            return ReCaptcha(secret)
 
     def post(self):
-        """Handles POST requests
+        """Handles POST requests.
 
         Required params:
             config=<configuration>
@@ -273,11 +213,12 @@ class Hisecon(RequestHandler):
             html (deprecated)
             format=(html,text,json) (new)
         """
-        if self.recaptcha.validate(self.response, remote_ip=self.remote_ip):
+        if self.recaptcha.validate(
+                self.response, remote_ip=self.query.get('remoteip')):
             self.logger.info('Got valid reCAPTCHA.')
             emails = list(self._emails(
                 self.sender, self.recipients,
-                self.subject, self.reply_to))
+                self.subject, self.query.get('reply_to')))
 
             try:
                 self.mailer.send(emails, fg=True)
