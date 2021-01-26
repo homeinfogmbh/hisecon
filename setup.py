430396849af11282fb316240a80fba420a3d8f1d
Richard Neumann <mail@richard-neumann.de>
2021-01-26 11:46:38 +0100

    Migrated to setuptools.

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 #! /usr/bin/env python3
+"""Install script."""
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='hisecon',
-    version='latest',
-    author='Richard Neumann',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info at homeinfo dot de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r dot neumann at homeinfo period de>',
+    install_requires=[
+        'configlib',
+        'emaillib',
+        'flask',
+        'recaptcha',
+        'werkzeug',
+        'wsgilib'
+    ],
     py_modules=['hisecon'],
-    description='HOMEINFO Secure Contact form')
+    description='HOMEINFO Secure Contact form'
+)
